bdf28c9a6f7e43191368d3ab05c6c49efffb2f33
JuliaKramskoy <julia.kramskoy@gmail.com>
2020-11-12 20:11:13 -0600

    * .gitignore: ASP progect
    * ASPproject.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * ASPproject.csproj:
    * site.js:
    * favicon.ico:
    * site.css:
    * Index.cshtml:
    * _ViewStart.cshtml:
    * ErrorViewModel.cs:
    * Helper.cs:
    * _ViewImports.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * HomeController.cs:
    * LICENSE:
    * launchSettings.json:
    * LICENSE.txt:
    * StudentController.cs:
    * jquery.js:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * Index.cshtml:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * Index.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap.bundle.js:
    * bootstrap-grid.css:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.js.map:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap-grid.min.css.map:
    * bootstrap.bundle.min.js.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ ASPproject/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPproject.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewBag.Students = new List<string>
+           {
+               "Mark","Rosy","John"
+           };
+
+            ViewData["Students"] = new List<string>
+                {
+               "Mark","Rosy","John"
+           };
+
+            return View();
+        }
+
+        public string GetStudentName() {
+            return "hello its a name method";
+        }
+    }
+}
---
+++ CourseRegistrationProgram/Helper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using CourseRegistrationProgram.Models;
+
+namespace CourseRegistrationProgram
+
+{
+    // including sample data to our tables
+    public static class Helper
+    {
+        public static List<StudentsModel> GetStudentsData()
+        {
+
+            var list = new List<StudentsModel>
+            {
+                new StudentsModel(){ FirstName = "Jone",LastName = "Simson",EmailAddress = "jone@gmail.com",PhoneNumber = "204-530-7765" },
+                new StudentsModel(){ FirstName = "Lesly",LastName = "Kery",EmailAddress = "lesly@gmail.com" ,PhoneNumber = "203-654-9985"},
+                new StudentsModel(){ FirstName = "Mark",LastName = "Lord",EmailAddress = "mark@gmail.com",PhoneNumber = "203-543-7654" },
+                new StudentsModel(){ FirstName = "Tom",LastName = "Morys",EmailAddress = "tom@gmail.com" ,PhoneNumber = "403-876-6543"},
+                new StudentsModel(){ FirstName = "More",LastName = "Kegen",EmailAddress = "more@gmail.com",PhoneNumber = "403-543-8760"}
+            };
+
+            return list;
+        }
+
+        public static List<CoursesModel> GetCourseData()
+        {
+            var list = new List<CoursesModel>
+            {
+                new CoursesModel(){ CourseNumber = "1",CourseName = "Accountant",CourseDescription = "Full stuck accountant" },
+                new CoursesModel(){ CourseNumber = "2",CourseName = "Project Manager",CourseDescription = "Learn how to planning, executing, monitoring, controlling and closing projects." },
+                new CoursesModel(){ CourseNumber = "3",CourseName = "Front End Developer",CourseDescription = "Practice of converting data to a graphical interface, through the use of HTML, CSS, and JavaScript." },
+                new CoursesModel(){ CourseNumber = "4",CourseName = "Dental assistant",CourseDescription = "This program provides the skills and knowledge required to assist the dentist" },
+            };
+
+            return list;
+        }
+
+        public static List<InstructorsModel> GetInstructorData()
+        {
+            var list = new List<InstructorsModel>
+            {
+                new InstructorsModel(){ FirstName = "Lary", LastName = "Lopster", EmailAddress = "lary@gmail.com", Course = "Accountant" },
+                new InstructorsModel(){ FirstName = "Mark", LastName = "Moren", EmailAddress = "mark@gmail.com", Course = "Project Manager" },
+                new InstructorsModel(){ FirstName = "Sam", LastName = "Keg", EmailAddress = "sam@gmail.com", Course = "Front End Developer" },
+                new InstructorsModel(){ FirstName = "Jerry", LastName = "Zeep", EmailAddress = "jerry@gmail.com", Course = "Dental assistant" },
+            };
+ 
+            return list;
+        }
+           
+    }
+}
